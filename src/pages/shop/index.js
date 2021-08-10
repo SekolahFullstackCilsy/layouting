7650299f4b2e6edc0cd9e715c73e316e771cf05d
Randy Vianda Putra <modulloe@gmail.com>
2021-08-10 20:49:02 +0700

    init: commit

---
+++ src/pages/shop/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../../layouts/Layout';
+
+const Shop = () => {
+  return (
+    <Layout>
+      <h1>Shop</h1>
+    </Layout>
+  )
+}
+
+export default Shop
